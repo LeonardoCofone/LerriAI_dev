4ce20451f5f8f76eea2d2250c9d8421c0e676d41
Leonardo Cofone <leonardo.cofone5@gmail.com>
2025-12-01 13:10:45 +0100

    Update app.js

--- pwa/app.js
+++ pwa/app.js
@@ -972,8 +972,6 @@ async function handleReauth() {
                 'https://www.googleapis.com/auth/userinfo.profile',
                 'https://www.googleapis.com/auth/drive.file',
                 'https://www.googleapis.com/auth/calendar.readonly',
-                'https://www.googleapis.com/auth/documents',
-                'https://www.googleapis.com/auth/spreadsheets',
                 'https://www.googleapis.com/auth/gmail.send'
             ].join(" "),
 
